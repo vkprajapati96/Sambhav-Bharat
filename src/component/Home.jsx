79e75c50bdf11df978721737485d1861c9daffef
vkprajapati96 <vkumars426675@gmail.com>
2025-10-15 17:48:56 +0530

    first commit

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -128,7 +128,7 @@ function Home() {
 
       {/* selcetion section */}
 
-        <div className=" mt-[6rem]">
+        <div className="bg-yellow-500 mb-8 mt-[6rem]">
       <div className="text-center text-4xl font-semibold text-white">
         Check Our Selection From
       </div>
@@ -257,7 +257,7 @@ function Home() {
 
 {/* services section */}
 
-<div className="flex flex-col bg-red-600 px-4 md:px-12  md:pt-24">
+<div className="flex flex-col bg-red-500 px-4 md:px-12  md:pt-24">
   <h1 className="text-white font-bold text-4xl text-center">Our Services</h1>
 
   <p className="text-white font-bold text-2xl text-center mt-4">
