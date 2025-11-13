929f2d196ed91c149dff5ff23a56d0c75863d149
vkprajapati96 <vkumars426675@gmail.com>
2025-11-13 15:05:06 +0530

    first commit

--- src/propertuespages/PrimeVista.jsx
+++ src/propertuespages/PrimeVista.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 
-import image1 from "../image/PrimeVistaImage/image1.jpeg";
+// import image1 from "../image/PrimeVistaImage/image1.jpeg";
 import image2 from "../image/PrimeVistaImage/image2.jpeg";
-import image3 from "../image/PrimeVistaImage/image3.jpeg";
+// import image3 from "../image/PrimeVistaImage/image3.jpeg";
 import image4 from "../image/PrimeVistaImage/image4.jpeg";
 import image5 from "../image/PrimeVistaImage/image5.jpeg";
 import image6 from "../image/PrimeVistaImage/image6.jpeg";
 import image7 from "../image/PrimeVistaImage/image7.jpeg";
-import image8 from "../image/PrimeVistaImage/image8.jpeg";
-import image9 from "../image/PrimeVistaImage/image9.jpeg";
+// import image8 from "../image/PrimeVistaImage/image8.jpeg";
+// import image9 from "../image/PrimeVistaImage/image9.jpeg";
 import image10 from "../image/PrimeVistaImage/image10.jpeg";
-import image11 from "../image/PrimeVistaImage/image11.jpeg";
-import image12 from "../image/PrimeVistaImage/image12.jpeg";
-import image13 from "../image/PrimeVistaImage/image13.jpeg";
-import image14 from "../image/PrimeVistaImage/image14.jpeg";
-import detail1 from "../image/PrimeVistaImage/detail1.jpeg";
+// import image11 from "../image/PrimeVistaImage/image11.jpeg";
+// import image12 from "../image/PrimeVistaImage/image12.jpeg";
+// import image13 from "../image/PrimeVistaImage/image13.jpeg";
+// import image14 from "../image/PrimeVistaImage/image14.jpeg";
+// import detail1 from "../image/PrimeVistaImage/detail1.jpeg";
 
 
 
